28f46456d4ec4c042f3515ea81de2d34b9e36d0d
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2023-09-24 11:57:40 -0300

    feat: implement the list filter module

--- lib/list_filter.ex
+++ lib/list_filter.ex
@@ -1,5 +1,17 @@
 defmodule ListFilter do
-  def hello do
-    :world
+  require Integer
+
+  def call(list) do
+    odd_values =
+      Enum.filter(list, fn item ->
+        case Integer.parse(item) do
+          {value, _} -> Integer.is_odd(value)
+          :error -> false
+        end
+      end)
+
+    list_length = length(odd_values)
+
+    list_length
   end
 end
--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -2,7 +2,25 @@ defmodule ListFilterTest do
   use ExUnit.Case
   doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "get the number of odd values" do
+      list = ["1", "3", "6", "43", "banana", "6", "abc"]
+
+      response = ListFilter.call(list)
+
+      expected_response = 3
+
+      assert response == expected_response
+    end
+
+    test "receive an empty list" do
+      list = []
+
+      response = ListFilter.call(list)
+
+      expected_response = 0
+
+      assert response == expected_response
+    end
   end
 end
